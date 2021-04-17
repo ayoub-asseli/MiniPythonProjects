6ce69d0156c92cff90c691e17aab7aa37e1e81e2
ayoub-asseli <59586260+ayoub-asseli@users.noreply.github.com>
2021-04-17 04:24:50 +0200

    Update myScraper2.py

--- AmazonScraper/myScraper2.py
+++ AmazonScraper/myScraper2.py
@@ -90,23 +90,97 @@ def get_links_for_each_item_format(soup):
         res.append(("https://www.amazon.fr/"+link, fort))
     return(res)
 
+def get_item_number_of_page(url):
+    soup = get_data_page(url)
+    div = soup.find('div', {'id': 'detailBullets_feature_div'})
+    ul = div.find('ul', {'class': 'a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list'})
+    li = ul.find_all(lambda tag: tag.name == 'li' and not tag.attrs)
+    number_of_pages1 = li[2].find('span', {'class': 'a-list-item'}).text.split()
+    number_of_pages2 = li[3].find('span', {'class': 'a-list-item'}).text.split()
+    test = li[0].find('span', {'class': 'a-list-item'}).text.split()
+    if 'ASIN' in test:
+        return number_of_pages2[2]
+    else:
+        return number_of_pages1[2]
+
+def get_file_size_kindle(url):
+    soup = get_data_page(url)
+    div = soup.find('div', {'id': 'detailBullets_feature_div'})
+    ul = div.find('ul', {'class': 'a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list'})
+    li = ul.find_all(lambda tag: tag.name == 'li' and not tag.attrs)
+    size_file = li[2].find('span', {'class': 'a-list-item'}).text.split()
+    return " ".join(size_file[4:])
+
+def get_item_dimension(url):
+    soup = get_data_page(url)
+    div = soup.find('div', {'id': 'detailBullets_feature_div'})
+    ul = div.find('ul', {'class': 'a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list'})
+    li = ul.find_all(lambda tag: tag.name == 'li' and not tag.attrs)
+    dim = li[len(li) - 1].find('span', {'class': 'a-list-item'}).text.split()
+    return " ".join(dim[2:])
+
 def get_item_broché_dimension(url, res):
     for elem in res:
         if 'Broché' in elem[1] and 'javascript' in elem[0]:
-            soup = get_data_page(url)
-            div = soup.find('div', {'id': 'detailBullets_feature_div'})
-            ul = div.find('ul', {'class': 'a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list'})
-            li = ul.find_all(lambda tag: tag.name == 'li' and not tag.attrs)
-            dim = li[len(li)-1].find('span', {'class': 'a-list-item'}).text.split()
+            dimension = get_item_dimension(url)
         elif 'Broché' in elem[1]:
             url = elem[0]
-            soup = get_data_page(url)
-            div = soup.find('div', {'id': 'detailBullets_feature_div'})
-            ul = div.find('ul', {'class': 'a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list'})
-            li = ul.find_all(lambda tag: tag.name == 'li' and not tag.attrs)
-            dim = li[len(li)-1].find('span', {'class': 'a-list-item'}).text.split()
-    return " ".join(dim[2:])
+            dimension = get_item_dimension(url)
+    return dimension
 
+def get_item_poche_dimension(url, res):
+    for elem in res:
+        if 'Poche' in elem[1] and 'javascript' in elem[0]:
+            dimension = get_item_dimension(url)
+        elif 'Poche' in elem[1]:
+            url = elem[0]
+            dimension = get_item_dimension(url)
+    return dimension
+
+def get_item_relié_dimension(url, res):
+    for elem in res:
+        if 'Relié' in elem[1] and 'javascript' in elem[0]:
+            dimension = get_item_dimension(url)
+        elif 'Relié' in elem[1]:
+            url = elem[0]
+            dimension = get_item_dimension(url)
+    return dimension
+
+def get_item_broché_number_of_pages(url, res):
+    for elem in res:
+        if 'Broché' in elem[1] and 'javascript' in elem[0]:
+            nb_pages = get_item_number_of_page(url)
+        elif 'Broché' in elem[1]:
+            url = elem[0]
+            nb_pages = get_item_number_of_page(url)
+    return nb_pages
+
+def get_item_poche_number_of_pages(url, res):
+    for elem in res:
+        if 'Poche' in elem[1] and 'javascript' in elem[0]:
+            nb_pages = get_item_number_of_page(url)
+        elif 'Poche' in elem[1]:
+            url = elem[0]
+            nb_pages = get_item_number_of_page(url)
+    return nb_pages
+
+def get_item_relié_number_of_pages(url, res):
+    for elem in res:
+        if 'Relié' in elem[1] and 'javascript' in elem[0]:
+            nb_pages = get_item_number_of_page(url)
+        elif 'Relié' in elem[1]:
+            url = elem[0]
+            nb_pages = get_item_number_of_page(url)
+    return nb_pages
+
+def get_item_file_size_kindle(url, res):
+    for elem in res:
+        if 'Kindle' in elem[1] and 'javascript' in elem[0]:
+            file_size = get_file_size_kindle(url)
+        elif 'Kindle' in elem[1]:
+            url = elem[0]
+            file_size = get_file_size_kindle(url)
+    return file_size
 
 ######## CLEANER FUNCTIONS ########
 
@@ -139,7 +213,43 @@ def generate_my_items_table(items_urls):
             continue
         try:
             links = get_links_for_each_item_format(soup)
+            nb_pages = get_item_broché_number_of_pages(item_url, links)
+            item.append(nb_pages)
+        except:
+            item.append('None')
+        try:
+            links = get_links_for_each_item_format(soup)
+            file_size = get_item_file_size_kindle(item_url, links)
+            item.append(file_size)
+        except:
+            item.append('None')
+        try:
+            links = get_links_for_each_item_format(soup)
+            nb_pages = get_item_poche_number_of_pages(item_url, links)
+            item.append(nb_pages)
+        except:
+            item.append('None')
+        try:
+            links = get_links_for_each_item_format(soup)
+            nb_pages = get_item_relié_number_of_pages(item_url, links)
+            item.append(nb_pages)
+        except:
+            item.append('None')
+        try:
+            links = get_links_for_each_item_format(soup)
             dimension = get_item_broché_dimension(item_url, links)
+            item.append(dimension)
+        except:
+            item.append('None')
+        try:
+            links = get_links_for_each_item_format(soup)
+            dimension = get_item_poche_dimension(item_url, links)
+            item.append(dimension)
+        except:
+            item.append('None')
+        try:
+            links = get_links_for_each_item_format(soup)
+            dimension = get_item_relié_dimension(item_url, links)
             item.append(dimension)
         except:
             item.append('None')
@@ -159,20 +269,19 @@ def generate_my_items_table(items_urls):
 
 # Get the first search page
 
-recherche = input('Veuillez entrer vos mots clés: ')
+recherche = 'livre recette minceur'
 url = "https://www.amazon.fr/s?k={}".format('+'.join(recherche.split()))
 soup = get_data_page(url)
 items_url = get_items_urls_from_all_pages(url)
-table = generate_my_items_table(items_url[:10])
+table = generate_my_items_table(items_url[:15])
 
-books = pd.DataFrame(table, columns=['Titres', 'Format Prix', 'Nombre de pages', 'Dimensions Format Broché', 'Dimensions Format Poche', 'Dimensions Format Relié', 'Evaluations (sur 5)', 'Nombre d\'évaluations', 'Liens' ])
-books.to_csv('aya.csv')
+books = pd.DataFrame(table, columns=['Titres', '[Formats, Prix]', 'Nombre de pages Format Broché', 'Taille du fichier Kindle' ,'Nombre de pages Format Poche', 'Nombre de pages Format Relié', 'Dimensions Format Broché', 'Dimensions Format Poche', 'Dimensions Format Relié', 'Evaluations (sur 5)', 'Nombre d\'évaluations', 'Liens' ])
+books.to_csv('test1.csv')
 
 
 # Test
 '''
-url = 'https://www.amazon.fr/R%C3%A9gime-C%C3%A9tog%C3%A8ne-efficacement-programme-d%C3%A9butants-ebook/dp/B087YTMPF3/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=1618574884&sr=8-317-spons'
+url = 'https://www.amazon.fr/dp/276195372X/ref=sspa_dk_detail_2?psc=1&pd_rd_i=276195372Xp13NParams&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUE1UDdXNFIyT0VLN00mZW5jcnlwdGVkSWQ9QTA2MDE2MTYzMjJKTUw3Vlk0RkdPJmVuY3J5cHRlZEFkSWQ9QTAyNTMwNDlDRktOWkdMVkFTQzEmd2lkZ2V0TmFtZT1zcF9kZXRhaWwyJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
 soup = get_data_page(url)
-links = get_links_for_each_item_format(soup)
-print(get_item_broché_dimension(soup, links))
+print(get_item_number_of_page(url))
 '''
