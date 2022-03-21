525b4c06264d9c9a42cd99fcd43140cc7129ee47
ayoub-asseli <59586260+ayoub-asseli@users.noreply.github.com>
2022-03-21 19:10:13 +0100

    Create RearrangeTheNumber.py

---
+++ RearrangeTheNumber.py
@@ -0,0 +1,15 @@
+"""
+To complete this challenge, write a function that accepts a number as a parameter. 
+The function should return a number that’s the difference between the largest and 
+smallest numbers that the digits can form in the number.
+For example, if the parameter is "213", the function should return "198", 
+which is the result of 123 subtracted from 321.
+"""
+
+import itertools
+
+def rearrange_the_number(number):
+	permutations = list(map(lambda x: int(''.join(x)) ,itertools.permutations(str(number))))
+	return max(permutations) - min(permutations)
+
+#print(rearrange_the_number(12282121212))
